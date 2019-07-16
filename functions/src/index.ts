4948b963bc39e105317999cd98ff7aa72687d7b7
keveightysev <keveightysev@gmail.com>
2019-07-16 10:22:55 -0500

    Very basic city list set

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as cities from './cities.json';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const loadCityData = functions.https.onRequest((req, res) => {
+  cities.forEach(async city => {
+    await db.collection('cities').add(city);
+  });
+});
