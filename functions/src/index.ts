9a827638592a84ff92b8c70cbe988c8cae262ac5
keveightysev <keveightysev@gmail.com>
2019-07-17 16:22:15 -0500

    Played around with city data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,8 +6,18 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-export const loadCityData = functions.https.onRequest((req, res) => {
-  cities.forEach(async city => {
-    await db.collection('cities').add(city);
+export const loadCityData = functions
+  .runWith({ timeoutSeconds: 300, memory: '1GB' })
+  .https.onRequest(async (req, res) => {
+    try {
+      console.log('Started import');
+      await cities.reduce(async (promise, city) => {
+        await promise;
+        await db.collection('baseCities').add(city);
+      }, Promise.resolve());
+      console.log(`Imported ${cities.length} cities`);
+      res.status(201);
+    } catch (err) {
+      console.log(err);
+    }
   });
-});
